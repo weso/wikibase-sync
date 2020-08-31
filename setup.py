df6ce79af1d1274dd136ec85f1a8eae1ab9c4a68
Alejandro González Hevia <alejandrgh11@gmail.com>
2020-08-31 20:31:38 +0200

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(curr_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='wbsync',
-    version='0.1.1',
+    version='0.1.2',
     packages=['wbsync', 'wbsync.external', 'wbsync.synchronization',
               'wbsync.triplestore', 'wbsync.util'],
     url='https://github.com/weso/rdf-wb-sync',
